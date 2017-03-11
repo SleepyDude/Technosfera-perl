d0dacf2b1b272a105a9a1fbb9b3c6208f4803f23
SleepyDude <ko_on@mail.ru>
2017-03-12 00:03:03 +0300

    analyze

--- homeworks/log-analyzer/bin/analyze.pl
+++ homeworks/log-analyzer/bin/analyze.pl
@@ -16,21 +16,43 @@ sub parse_file {
     my $file = shift;
 
     # you can put your code here
-
+    my %res;
     my $result;
     open my $fd, "-|", "bunzip2 < $file" or die "Can't open '$file': $!";
     while (my $log_line = <$fd>) {
 
         # you can put your code here
         # $log_line contains line from log file
-        my %line;
         my %time;
-        $line{"IP"} = $log_line =~ /(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)/;
-        ($time{"day"}, $time{"month"}, $time{"year"}, 
-         $time{"hours"}, $time{"minutes"}, $time{"seconds"}) = $log_line =~ /\[(\d+)\/(.+)\/(\d+):(\d+):(\d+):(\d+).+\]/;
+        $log_line =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/;
+        my $ip = $1;
+        $res{$ip}->{"count"} += 1;
+        $log_line =~ /\[(\d+)\/(.+)\/(\d+):(\d+):(\d+):(\d+).+\]/;
+        (my $day,my $month,my $year,my $hours,my $minutes,my $seconds) = ($1, $2, $3, $4, $5, $6);
+        my $sec = $seconds + $minutes * 60 + $hours * 3600 + $day * 3600 * 24;
+        if ($sec < $res{$ip}->{"times"}->[0]) {
+            unshift @{ $res{$ip}->{"times"} }, $sec;
+        }
+        elsif ($sec > $res{$ip}->{"times"}->[-1]) {
+            push @{ $res{$ip}->{"times"} }, $sec;
+        }
+        #$res{$ip}->{"time"} = \%time;
+        $log_line =~ /\s(\d{3})\s(\d+)/;
+        my $status = $1;
+        my $data = $2;
+        $res{$ip}->{"status"}->{$status} += $data;
+        $log_line =~ /"(\d+(\.\d+)?)"/;
+        my $zipC = $1;
+        if ($status == 200) {
+            $res{$ip}->{"data"} += $zipC * $data;
+        }
     }
     close $fd;
 
+    for my $key (keys %res) {
+        $res{$key}->{"avg"} = $res{$key}->{"count"} * 60 / ($res{$key}->{"times"}->[-1] - $res{$key}->{"times"}->[0] + 1)
+    }
+    $result = \%res;
     # you can put your code here
 
     return $result;
